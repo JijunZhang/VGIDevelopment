6f707db2f37ac0b00f3edec519fa61b1c06e366f
qianlong <372972084@qq.com>
2017-05-26 10:58:29 +0800

    recoding mongo file, to be continued.

--- src/mongo/databaseconn.js
+++ src/mongo/databaseconn.js
@@ -16,38 +16,36 @@ var databaseconn = {
 };
 //用户认证
 var authorUri = {
-    //Data: 'mongodb://admin:!admin_1@120.26.205.190:27017/Data',
-    Data: 'mongodb://172.16.101.153:27017/mean-book',
+    VGIDatabaseDev: 'mongodb://VGISeverPro:vgiserver$0525@172.16.101.153:27017/VGIDatabaseDev',
 };
 //数据库连接字符串
 var dataUri = {
-    //jiayu: 'mongodb://admin:!admin_2@120.26.205.190:27017/jiayu',
-    Data: 'mongodb://172.16.101.153:27017/mean-book',
+    VGIDatabaseDev: 'mongodb://VGISeverPro:vgiserver$0525@172.16.101.153:27017/VGIDatabaseDev',
 };
 //认证接口初始化
-// for (var authorName in authorUri) {
-//     (function (name) {
-//         databaseconn.auth[name] = mongoose.createConnection(authorUri[name], {
-//             server: {
-//                 socketOptions: {
-//                     keepAlive: 1
-//                 }
-//             },
-//             replset: {
-//                 socketOptions : {
-//                     keepAlive: 1
-//                 }
-//             }
-//         }, function (err, data) {
-//             if (!!err) {
-//                 console.log('连接数据库失败' + JSON.stringify(err));
-//             } else {
-//                 console.log(name + ' 数据库连接成功');
-//                 authenticment.inilization(databaseconn.auth[name]);
-//             }
-//         });
-//     })(authorName);
-// }
+for (var authorName in authorUri) {
+    (function (name) {
+        databaseconn.auth[name] = mongoose.createConnection(authorUri[name], {
+            server: {
+                socketOptions: {
+                    keepAlive: 1
+                }
+            },
+            replset: {
+                socketOptions : {
+                    keepAlive: 1
+                }
+            }
+        }, function (err, data) {
+            if (!!err) {
+                console.log('连接数据库失败' + JSON.stringify(err));
+            } else {
+                console.log(name + ' 数据库连接成功');
+                authenticment.inilization(databaseconn.auth[name]);
+            }
+        });
+    })(authorName);
+}
 //数据接口初始化
 for (var dataName in dataUri) {
     (function (name) {
